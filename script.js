323ed5bcdb856e375802a5b2a5228369fb5d4f75
Rubens Escobar <escobarbinho5@gmail.com>
2024-10-30 12:34:45 -0300

    more corrections

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemInput = document.getElementById('itemInput');
 const addItemButton = document.getElementById('addItemButton');
 const shoppingList = document.getElementById('shoppingList');
+const footer = document.querySelector('footer')
 
 
 function addItem() {
@@ -25,10 +26,14 @@ function addItem() {
         const deleteButton = document.createElement('button');
         deleteButton.classList.add('delete-btn');
         deleteButton.innerHTML = '<img src="./assets/icons/Frame (3).svg" alt="Deletar">';
+        
+        
+        
 
         
         deleteButton.addEventListener('click', function() {
-            shoppingList.removeChild(itemDiv); 
+            shoppingList.removeChild(itemDiv);
+            footer.classList.add("show-result"); 
         });
 
         
@@ -46,3 +51,16 @@ function addItem() {
 
 
 addItemButton.addEventListener('click', addItem);
+
+// function deleteButton {
+//     try {
+//       // Aplica a classe que exibe o footer para mostrar o resultado.
+//       footer.classList.add("show-result")
+//     } catch {
+//       // Remove a classe do footer removendo ele da tela.
+//       footer.classList.remove("show-result")
+      
+//       console.log(error)
+//       alert("Não foi possível converter. Tente novamente mais tarde.")
+//     }
+//   }
